42d50f3baae7c42914625402005bc94a9a974d69
xing215 <dev@xing512.onmicrosoft.com>
2024-08-10 11:51:31 +0700

    Complete deque

---
+++ src/deque.h
@@ -0,0 +1,35 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
+template <class DataType>
+class DequeNode
+{
+    friend class Deque;
+private:
+    DataType data;
+    DequeNode *next;
+public:
+    DequeNode(DataType in_data, DequeNode *in_next = nullptr);
+};
+
+template <class DataType>
+class Deque
+{
+private:
+    DequeNode<DataType> *head;
+    DequeNode<DataType> *tail;
+    int size;
+public:
+    Deque();
+    ~Deque();
+    bool isEmpty();
+    void push_back(DataType data);
+    void push_front(DataType data);
+    void pop_back();
+    void pop_front();
+    DataType front();
+    DataType back();
+    void clear();
+};
+
+#endif
